77c99f258ca2a647f76794c216311a5c02147f2c
Wielton <grantwielgosz@gmail.com>
2022-03-25 14:53:19 -0600

    Everything is working correctly.  I didn't use checkboxes, instead I used a button for each selection. I may try adding that feature in before due date.

--- js/selection.js
+++ js/selection.js
@@ -1,11 +1,40 @@
-function reselect() {
-    Cookies.remove('Cappuccino');
-    window.location.href='/index.html';
-}
+// How can I make the selection dynamic 
+// so that the cookie.remove can accept a dynamic value?
 
+
+
+function reselect() {
+//  Cookies.remove(userSelection);
+    for (let i = 0; i < cookieArray.length; i++) {
+        if (cookieArray[i] = userSelection1) {
+            Cookies.remove('Cappuccino');
+            
+        } else if (cookieArray[i] = userSelection2) {
+            Cookies.remove('Pastel');
+            
+        } else if (cookieArray[i] = userSelection3) {
+            Cookies.remove('Program');
+            
+        }
+    }window.location.href='/index.html';
+    
+}
+// 1. userselection[i] is the fetched cookie but I need to make this new 
+//     variable dynamic so I can use it as the argument to Cookies.remove(userSelection)
+//  For now I had to create a conditional block to remove a specific cookie
 let userSelection1 = Cookies.get('Cappuccino');
-document.getElementById('choiceName').innerText=userSelection1;
 let userSelection2 = Cookies.get('Pastel');
-document.getElementById('choiceName').innerText=userSelection2;
 let userSelection3 = Cookies.get('Program');
-document.getElementById('choiceName').innerText=userSelection3;
+let cookieArray = [userSelection1, userSelection2, userSelection3];
+
+
+//  Create a forLoop to iterate through cookieArray[i] and 
+//  a conditionl to inject innerText into ('choicename');
+for (let n = 0; n < cookieArray.length; n++) {
+    if (cookieArray[n] = userSelection1) {
+        document.getElementById('choiceName').innerText='Cappuccino';
+    } else if (cookieArray[n] = userSelection2) {
+        document.getElementById('choiceName').innerText='Pastel';
+}else if (cookieArray[n] = userSelection3) {
+    document.getElementById('choiceName').innerText='Program';
+}}
